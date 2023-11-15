f094738bb42449fa7d073571a80d2646fa733029
Ramahatafandry_Maeva <henintsoa.ramahatafandry@etu.sorbonne-universite.fr>
2023-11-15 02:05:03 +0100

    partie 1.1 finie

--- algo1.py
+++ algo1.py
@@ -1,13 +1,43 @@
-M = 5
-def case_possible(j : int, l: int, s):
-    if l = 0:
-        return True
-    elif l >= 1:
-        if j < s[l] - 1:
-            return False
-        elif j == s[l] - 1:
-            if j != M-1:
-                return True
-        else:
-            return case_possible(j+1, l-1, s)
+from config import *
 
+def is_case_possible(j : int, l: int, s: list):
+    """
+    j : Nombre de colonnes de la ligne testée
+    l : Nombre de cases actuel dans le bloc de la sequence
+    s : séquence à vérifier (si elle est coloriable)
+    """
+    if l == 0:
+        return True
+    
+    if l >= 1:
+        if j < s[l-1]:
+            return False
+        
+        if j == s[l-1]:
+            if l == 1:
+                return True
+            else:
+                return False
+        
+        if j > s[l-1]:
+            return is_case_possible(j-1, l, s) or is_case_possible(j-s[l-1]-1, l-1, s)
+    
+##### TEST CASES #####
+
+#j = 5, l = 0, s = []
+assert(is_case_possible(5, 0, []) == True) 
+
+#j = 2, l = 2, s = [3, 2, 1]
+assert(is_case_possible(2, 2, [3, 2, 1]) == False)
+
+#j = 3, l = 2, s = [3, 2, 1]
+assert(is_case_possible(3, 2, [3, 2, 1]) == False)
+
+#j = 5, l = 3, s = [1, 1, 1]
+assert(is_case_possible(5, 3, [1, 1, 1]) == True)
+
+#j = 3, l = 1, s = [3]
+assert(is_case_possible(3, 1, [3]) == True)
+
+#j = 5, l = 2, s = [3, 2, 1]
+assert(is_case_possible(5, 2, [3, 2, 1]) == False)
---
+++ config.py
@@ -0,0 +1,4 @@
+BLANC = 0
+NOIR = 1
+
+seq1 = []
