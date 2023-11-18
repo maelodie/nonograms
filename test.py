2ab836b36bd51cd22be062e3cab5106b0774dbb3
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-11-18 15:12:31 +0100

    restructuration

--- test.py
+++ test.py
@@ -1,44 +1,70 @@
-from coloriage import est_coloriable_rec
+from coloriage import est_coloriable_rec, est_coloriable_rec_2
 from config import *
 
 ##################
 # Test cases Q4
 ##################
-
-# j = 5, l = 0, s = []
 M = 6
-memo = creer_tab(M,M,VIDE) # matrice de mémoisation
-assert est_coloriable_rec(M-1, 0, [], memo) == True
+seq_1 = []
+seq_2 = [1,2,1]
+seq_3 = [2,2]
+seq_4 = [6]
+seq_5 = [4,2]
+seq_6 = [3,2,1]
+seq_7 = [1,1,1,1,1,1]
 
-# j = 3, l = 2, s = [2, 1]
-M = 4
-memo = creer_tab(M,M,VIDE) 
-assert est_coloriable_rec(M-1, 2, [2, 1], memo) == False
 
-# j = 2, l = 2, s = [3, 2, 1]
-M = 3
-memo = creer_tab(M,M,VIDE) 
-assert est_coloriable_rec(2, 2, [3, 2, 1], memo) == False
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec(M-1, 0, seq_1, memo) == True
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec(M-1, 3, seq_2, memo) == True
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec(M-1, 2, seq_3, memo) == True
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec(M-1, 1, seq_4, memo) == True
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec(M-1, 2, seq_5, memo) == False
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec(M-1, 3, seq_6, memo) == False
+memo = creer_tab(M, M+1,VIDE)
+assert est_coloriable_rec(M-1, 6, seq_7, memo) == False
 
-# j = 3, l = 2, s = [3, 2, 1]
-M = 4
-memo = creer_tab(M,M,VIDE) 
-assert est_coloriable_rec(3, 2, [3, 2, 1], memo) == False
+print("Tous les testes sont passés pour est_coloriable_rec !")
+##################
 
-# j = 5, l = 3, s = [1, 1, 1]
-M = 6
-memo = creer_tab(M,M,VIDE) 
-assert est_coloriable_rec(5, 3, [1, 1, 1], memo) == True
+##################
+# Test cases Q5
+##################
+M = 4 
 
-# j = 3, l = 1, s = [3]
-M = 4
-memo = creer_tab(M,M,VIDE) 
-assert est_coloriable_rec(3, 1, [3], memo) == True
 
-# j = 5, l = 2, s = [3, 2, 1]
-M = 6
-memo = creer_tab(M,M,VIDE) 
-assert est_coloriable_rec(5, 2, [3, 2, 1], memo) == False
+seq_1 = []
+cases_colorees = [BLANC, BLANC, BLANC, BLANC]
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec_2(M-1, 0, seq_1, memo, cases_colorees) == True
+cases_colorees = [BLANC, BLANC, NOIR, BLANC]
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec_2(M-1, 0, seq_1, memo, cases_colorees) == False
 
-print("Tous les testes sont passés !")
-# ##################
+seq_2 = [6]
+cases_colorees = [VIDE, BLANC, NOIR, BLANC]
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec_2(M-1, 1, seq_2, memo, cases_colorees) == False
+
+seq_3 = [3]
+cases_colorees = [BLANC, BLANC, NOIR, BLANC]
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec_2(M-1, 1, seq_3, memo, cases_colorees) == False
+
+seq_4 = [2]
+cases_colorees = [NOIR, NOIR, BLANC, VIDE]
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec_2(M-1, 1, seq_4, memo, cases_colorees) == True
+
+seq_5 = [2, 1]
+cases_colorees = [VIDE, VIDE, BLANC, NOIR]
+memo = creer_tab(M, M,VIDE)
+assert est_coloriable_rec_2(M-1, 2, seq_5, memo, cases_colorees) == True
+
+print("Tous les testes sont passés pour est_coloriable_rec_2 !")
+##################
