82a348899cef380988a91db847d1bbc7bb47e971
Ramahatafandry_Maeva <henintsoa.ramahatafandry@etu.sorbonne-universite.fr>
2023-11-22 12:02:53 +0100

    indication de l'affichage dans le main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from methode_incomplete import *
 from methode_complete import *
 from config import *
-
+place_affichage = "Les affichages des solutions se trouvent dans le dossier /instances sous le format <n° instance>.txt<methode>.jpeg"
 type = input("Entrer sur 'I' pour la méthode incomplète et 'C' pour la méthode complète sur toutes les instances possibles.\nPour une instance particulière, appuyer 'A'\n")
 
 if type == 'I':
@@ -13,6 +13,7 @@ if type == 'I':
         fin = time.time()
         temps_exec.append((fin - debut))
     print(timeFormat(temps_exec))
+    print(place_affichage)
 
 elif type == 'C':
     temps_exec = []            
@@ -23,6 +24,7 @@ elif type == 'C':
         fin = time.time()
         temps_exec.append((fin - debut))
     print(timeFormat(temps_exec))
+    print(place_affichage)
 
 elif type ==  'A':
     methode = int(input("Entrer 0 pour la méthode incomplète et 1 pour la méthode complète\n"))
@@ -33,7 +35,9 @@ elif type ==  'A':
         res = propagation_incomplete(instance)
         fin = time.time()
         exec = fin - debut
-        print("Le temps d'exécution est:", timeFormatSeconds(exec))
+        print("Le temps d'exécution est:", timeFormatSeconds(exec), exec, "s")
+        print(place_affichage)
+
 
     else:
         instance = ("instances/"+ str(instance_number) +".txt")
@@ -41,5 +45,7 @@ elif type ==  'A':
         res = propagation_complete(instance)
         fin = time.time()
         exec = fin - debut
-        print("Le temps d'exécution est:", timeFormatSeconds(exec))
+        print("Le temps d'exécution est:", timeFormatSeconds(exec), exec, "s")
+        print(place_affichage)
+
 
